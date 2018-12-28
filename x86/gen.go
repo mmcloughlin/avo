238771f86f8768da4428aa3cdad5a0f56346a460
Michael McLoughlin <mmcloughlin@gmail.com>
2018-12-28 15:47:38 -0800

    x86: remove unused error type

--- x86/gen.go
+++ x86/gen.go
@@ -1,8 +1,4 @@
 package x86
 
-import "errors"
-
-var ErrBadOperandTypes = errors.New("bad operand types")
-
 //go:generate avogen -output zctors.go ctors
 //go:generate avogen -output zctors_test.go ctorstest
