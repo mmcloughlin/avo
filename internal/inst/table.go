f9a67bb2c01ccf8b8fe8c6200c391594baf5b883
Michael McLoughlin <mmcloughlin@gmail.com>
2019-01-05 11:43:39 -0800

    internal/inst: doc exported symbols (#9)

--- internal/inst/table.go
+++ internal/inst/table.go
@@ -3,6 +3,8 @@ package inst
 //go:generate avogen -bootstrap -data ../data -output ztable.go godata
 //go:generate avogen -bootstrap -data ../data -output ztable_test.go godatatest
 
+// Lookup returns the instruction with the given opcode. Boolean return value
+// indicates whether the instruction was found.
 func Lookup(opcode string) (Instruction, bool) {
 	for _, i := range Instructions {
 		if i.Opcode == opcode {
