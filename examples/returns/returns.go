cdf63e263d9952c9cb2266793935286c25ed3220
Michael McLoughlin <mmcloughlin@gmail.com>
2019-01-13 11:09:54 -0800

    lint: spelling mistake

--- examples/returns/returns.go
+++ examples/returns/returns.go
@@ -1,6 +1,6 @@
 package returns
 
-// Struct is used to deomonstrate writing struct return values.
+// Struct is used to demonstrate writing struct return values.
 type Struct struct {
 	Word  uint16
 	Point [2]float64
