5d609353d1222c858df4b7af08c9ced77ae9fc7f
Michael McLoughlin <mmcloughlin@gmail.com>
2019-01-21 22:36:49 -0800

    docstrings for linter

--- examples/ext/ext/ext.go
+++ examples/ext/ext/ext.go
@@ -1,5 +1,7 @@
+// Package ext is used as a target package in the external types example.
 package ext
 
+// Struct is the target type in the external types example.
 type Struct struct {
 	A [3]uint16
 	B byte
--- examples/ext/stub.go
+++ examples/ext/stub.go
@@ -2,4 +2,5 @@ package ext
 
 import "github.com/mmcloughlin/avo/examples/ext/ext"
 
+// ExtStructFieldB returns field B.
 func ExtStructFieldB(e ext.Struct) byte
