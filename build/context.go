b96fcf0ced6e6b203a0eda2d5a0bc1d85b44763d
Michael McLoughlin <mmcloughlin@gmail.com>
2019-06-13 11:12:45 -0700

    lint: use minimal packages.Need* flags

--- build/context.go
+++ build/context.go
@@ -35,7 +35,7 @@ func NewContext() *Context {
 // Package sets the package the generated file will belong to. Required to be able to reference types in the package.
 func (c *Context) Package(path string) {
 	cfg := &packages.Config{
-		Mode: packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles | packages.NeedImports | packages.NeedDeps | packages.NeedTypes | packages.NeedTypesSizes | packages.NeedSyntax | packages.NeedTypesInfo,
+		Mode: packages.NeedTypes | packages.NeedDeps,
 	}
 	pkgs, err := packages.Load(cfg, path)
 	if err != nil {
--- gotypes/signature_test.go
+++ gotypes/signature_test.go
@@ -51,7 +51,7 @@ func TestLookupSignatureErrors(t *testing.T) {
 func LoadPackageTypes(t *testing.T, path string) *types.Package {
 	t.Helper()
 	cfg := &packages.Config{
-		Mode: packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles | packages.NeedImports | packages.NeedDeps | packages.NeedTypes | packages.NeedTypesSizes,
+		Mode: packages.NeedTypes,
 	}
 	pkgs, err := packages.Load(cfg, path)
 	if err != nil {
