d4c787e57c55e7edf60295345d0d5f04aceb25f7
Michael McLoughlin <mmcloughlin@gmail.com>
2020-01-22 18:28:11 -0800

    wip

--- pass/reg_test.go
+++ pass/reg_test.go
@@ -1,16 +1,31 @@
 package pass_test
 
 import (
+	"reflect"
 	"testing"
 
-	"github.com/mmcloughlin/avo/ir"
-	"github.com/mmcloughlin/avo/reg"
-
-	"github.com/mmcloughlin/avo/pass"
-
 	"github.com/mmcloughlin/avo/build"
+	"github.com/mmcloughlin/avo/ir"
 	"github.com/mmcloughlin/avo/operand"
+	"github.com/mmcloughlin/avo/pass"
+	"github.com/mmcloughlin/avo/reg"
 )
+
+func TestZeroExtend32BitOutputs(t *testing.T) {
+	i := &ir.Instruction{
+		Outputs: []operand.Op{reg.R8B, reg.R9W, reg.R10L, reg.R11},
+	}
+
+	err := pass.ZeroExtend32BitOutputs(i)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expect := []operand.Op{reg.R8B, reg.R9W, reg.R10, reg.R11}
+	if !reflect.DeepEqual(expect, i.Outputs) {
+		t.Fatalf("\n   got: %v\nexpect: %v", i.Outputs, expect)
+	}
+}
 
 func TestLivenessBasic(t *testing.T) {
 	// Build: a = 1, b = 2, a = a+b
