69dd5cb9bbcc326969108f004b29fa1ae1a12db9
Michael McLoughlin <mmcloughlin@gmail.com>
2020-01-27 14:49:45 -0800

    fix minrepro test

--- tests/fixedbugs/issue100/minrepro/doc.go
+++ tests/fixedbugs/issue100/minrepro/doc.go
@@ -1,2 +1,2 @@
-// Package issue100 contains a reproducer for a bug in aliased register allocation.
-package issue100
+// Package minrepro contains a minimal reproducer for the aliased register allocation bug in issue 100.
+package minrepro
