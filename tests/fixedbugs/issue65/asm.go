742c4fca142e4e0b5c3b114a73beeec37b39b0f5
Michael McLoughlin <mmcloughlin@gmail.com>
2020-01-27 14:54:20 -0800

    fix issue65 test

--- tests/fixedbugs/issue65/asm.go
+++ tests/fixedbugs/issue65/asm.go
@@ -1,11 +1,4 @@
-// +build generate
-
-//go:generate go run $GOFILE
-
-// Regression test for a bug where casting a physical register would give the
-// error "non physical register found".
-//
-// See: https://github.com/mmcloughlin/avo/issues/65#issuecomment-576850145
+// +build ignore
 
 package main
 
