c0de94efddf8f4a2f0375b88fd81923846aeddde
Michael McLoughlin <mmcloughlin@gmail.com>
2020-12-31 22:30:55 -0800

    tests: zeroing test

--- tests/alloc/zeroing/zeroing_test.go
+++ tests/alloc/zeroing/zeroing_test.go
@@ -9,10 +9,21 @@ import (
 //go:generate go run asm.go -out zeroing.s -stubs stub.go
 
 func TestZeroing(t *testing.T) {
+	const (
+		n      = 32
+		expect = n * (n + 1) / 2
+	)
+
 	if !cpu.X86.HasAVX512F {
 		t.Skip("require AVX512F")
 	}
 
 	var got [8]uint64
 	Zeroing(&got)
+
+	for i := 0; i < 8; i++ {
+		if got[i] != expect {
+			t.Errorf("got[%d] = %d; expect %d", i, got[i], expect)
+		}
+	}
 }
