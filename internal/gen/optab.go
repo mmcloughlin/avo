d818c7dd76f92079e70385c7f60367765bbc190d
Michael McLoughlin <mmcloughlin@gmail.com>
2021-01-18 13:01:32 -0800

    internal/gen,x86: optab types, arity, suffixes

--- internal/gen/optab.go
+++ internal/gen/optab.go
@@ -20,14 +20,34 @@ func (t *optab) Generate(is []inst.Instruction) ([]byte, error) {
 	t.Printf("// %s\n\n", t.cfg.GeneratedWarning())
 	t.Printf("package x86\n\n")
 
+	// Arity.
+	t.arity(is)
+
 	// Operand types and implicit registers.
 	t.operandTypes(is)
 	t.implicitRegisters(is)
+
+	// Suffixes.
+	t.suffixes(is)
 
 	// Opcodes table.
 	t.opcodes(is)
 
 	return t.Result()
+}
+
+func (t *optab) arity(is []inst.Instruction) {
+	max := 0
+	for _, i := range inst.Instructions {
+		for _, f := range i.Forms {
+			a := len(f.Operands) + len(f.ImplicitOperands)
+			if a > max {
+				max = a
+			}
+		}
+	}
+
+	t.Printf("const MaxArity = %d\n", max)
 }
 
 func (t *optab) operandTypes(is []inst.Instruction) {
@@ -42,6 +62,14 @@ func (t *optab) implicitRegisters(is []inst.Instruction) {
 	e := &enum{name: "ImplicitRegister"}
 	for _, r := range inst.ImplicitRegisters(is) {
 		e.values = append(e.values, api.ImplicitRegisterIdentifier(r))
+	}
+	e.Print(&t.Generator)
+}
+
+func (t *optab) suffixes(is []inst.Instruction) {
+	e := &enum{name: "Suffix"}
+	for _, s := range inst.UniqueSuffixes(is) {
+		e.values = append(e.values, s.String())
 	}
 	e.Print(&t.Generator)
 }
--- internal/inst/table.go
+++ internal/inst/table.go
@@ -19,7 +19,6 @@ func Lookup(opcode string) (Instruction, bool) {
 // OperandTypes returns all the operand types that appear in the provided
 // instructions.
 func OperandTypes(is []Instruction) []string {
-	// Collect set.
 	set := map[string]bool{}
 	for _, i := range is {
 		for _, f := range i.Forms {
@@ -28,22 +27,12 @@ func OperandTypes(is []Instruction) []string {
 			}
 		}
 	}
-
-	// Convert to sorted slice.
-	types := make([]string, 0, len(set))
-	for t := range set {
-		types = append(types, t)
-	}
-
-	sort.Strings(types)
-
-	return types
+	return sortedslice(set)
 }
 
 // ImplicitRegisters returns all the registers that appear as implicit operands
 // in the provided instructions.
 func ImplicitRegisters(is []Instruction) []string {
-	// Collect set.
 	set := map[string]bool{}
 	for _, i := range is {
 		for _, f := range i.Forms {
@@ -52,14 +41,45 @@ func ImplicitRegisters(is []Instruction) []string {
 			}
 		}
 	}
+	return sortedslice(set)
+}
 
-	// Convert to sorted slice.
-	regs := make([]string, 0, len(set))
-	for r := range set {
-		regs = append(regs, r)
+// UniqueSuffixes returns all the non-empty suffixes that appear in the provided
+// instructions.
+func UniqueSuffixes(is []Instruction) []Suffix {
+	// Collect set.
+	set := map[Suffix]bool{}
+	for _, i := range is {
+		for _, f := range i.Forms {
+			for _, suffixes := range f.SupportedSuffixes() {
+				for _, suffix := range suffixes {
+					set[suffix] = true
+				}
+			}
+		}
 	}
 
-	sort.Strings(regs)
+	// Convert to sorted slice.
+	suffixes := make([]Suffix, 0, len(set))
+	for suffix := range set {
+		suffixes = append(suffixes, suffix)
+	}
 
-	return regs
+	sort.Slice(suffixes, func(i, j int) bool {
+		return suffixes[i] < suffixes[j]
+	})
+
+	return suffixes
+}
+
+// sortedslice builds a sorted slice of strings from a set.
+func sortedslice(set map[string]bool) []string {
+	ss := make([]string, 0, len(set))
+	for s := range set {
+		ss = append(ss, s)
+	}
+
+	sort.Strings(ss)
+
+	return ss
 }
