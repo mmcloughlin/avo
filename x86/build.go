0573a8796fbadd4fa7a0e81bec096f15d569e555
Michael McLoughlin <mmcloughlin@gmail.com>
2021-02-21 18:42:16 -0800

    wip

---
+++ x86/build.go
@@ -0,0 +1,50 @@
+package x86
+
+import (
+	"errors"
+
+	"github.com/mmcloughlin/avo/ir"
+	"github.com/mmcloughlin/avo/operand"
+)
+
+// BuildInstruction constructs an instruction object from a list of acceptable
+// forms, and given input operands and suffixes.
+func BuildInstruction(forms []Form, suffixes Suffixes, ops []operand.Op) (*ir.Instruction, error) {
+	for i := range forms {
+		f := &forms[i]
+		if match(f, suffixes, ops) {
+			return build(f, suffixes, ops)
+		}
+	}
+	return nil, errors.New("bad operands")
+}
+
+func match(f *Form, suffixes Suffixes, ops []operand.Op) bool {
+	// Match suffix.
+	accept := f.SuffixesClass.SuffixesSet()
+	if !accept[suffixes] {
+		return false
+	}
+
+	// Match operands.
+	if len(ops) != int(f.Arity) {
+		return false
+	}
+
+	for i, op := range ops {
+		t := OperandType(f.Operands[i].Type)
+		if !t.Match(op) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func build(f *Form, suffixes Suffixes, ops []operand.Op) (*ir.Instruction, error) {
+	i := &ir.Instruction{
+		Opcode: f.Opcode.String(),
+	}
+
+	return i, nil
+}
