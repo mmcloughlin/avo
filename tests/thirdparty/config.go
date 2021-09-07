b0c444758894488183504fc9ec8bed2647aa793b
Michael McLoughlin <mmcloughlin@gmail.com>
2021-09-07 11:20:56 -0700

    more flexible third-party test defn

--- tests/thirdparty/config.go
+++ tests/thirdparty/config.go
@@ -8,13 +8,24 @@ import (
 	"path/filepath"
 )
 
+type Context struct {
+	AvoDirectory        string
+	RepositoryDirectory string
+}
+
+type Step struct {
+	WorkingDirectory string     `json:"dir"`
+	Commands         [][]string `json:"commands"`
+}
+
 // Package defines an integration test based on a third-party package using avo.
 type Package struct {
-	ImportPath string     `json:"import_path"` // package import path
-	Version    string     `json:"version"`     // git sha, tag or branch
-	Generate   [][]string `json:"generate"`    // generate commands to run
-	Dir        string     `json:"dir"`         // working directory for generate commands
-	Test       string     `json:"test"`        // test path relative to repo root (if empty defaults to ./...)
+	ImportPath string  `json:"import_path"` // package import path
+	Version    string  `json:"version"`     // git sha, tag or branch
+	Module     string  `json:"module"`      // path to module file
+	Setup      []*Step `json:"setup"`       // setup commands to run
+	Generate   []*Step `json:"generate"`    // generate commands to run
+	Test       string  `json:"test"`        // test path relative to repo root (if empty defaults to ./...)
 }
 
 // Name returns the package name.
@@ -33,6 +44,45 @@ func (p Package) TestPath() string {
 		return "./..."
 	}
 	return p.Test
+}
+
+func (p Package) Steps(c *Context) []*Step {
+	var steps []*Step
+
+	// Optional setup.
+	steps = append(steps, p.Setup...)
+
+	// Replace avo dependency.
+	const invalid = "v0.0.0-00010101000000-000000000000"
+	moddir := filepath.Dir(p.Module)
+	modfile := filepath.Base(p.Module)
+	steps = append(steps, &Step{
+		WorkingDirectory: moddir,
+		Commands: [][]string{
+			{"go", "mod", "edit", "-modfile=" + modfile, "-require=github.com/mmcloughlin/avo@" + invalid},
+			{"go", "mod", "edit", "-modfile=" + modfile, "-replace=github.com/mmcloughlin/avo=" + c.AvoDirectory},
+			{"go", "mod", "tidy", "-modfile=" + modfile},
+		},
+	})
+
+	// Run generation.
+	steps = append(steps, p.Generate...)
+
+	// Display changes.
+	steps = append(steps, &Step{
+		Commands: [][]string{
+			{"git", "-C", c.RepositoryDirectory, "diff"},
+		},
+	})
+
+	// Tests.
+	steps = append(steps, &Step{
+		Commands: [][]string{
+			{"go", "test", p.TestPath()},
+		},
+	})
+
+	return steps
 }
 
 // LoadPackages loads a list of package configurations from JSON format.
