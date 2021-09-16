ac9562db9719aba0cd86e419de726f64e518af16
Michael McLoughlin <mmcloughlin@gmail.com>
2021-09-15 23:15:07 -0700

    test coverage

--- tests/thirdparty/config_test.go
+++ tests/thirdparty/config_test.go
@@ -5,6 +5,84 @@ import (
 	"testing"
 )
 
+func TestValidateErrors(t *testing.T) {
+	cases := []struct {
+		Name           string
+		Item           interface{ Validate() error }
+		ErrorSubstring string
+	}{
+		{
+			Name:           "step_missing_name",
+			Item:           &Step{},
+			ErrorSubstring: "missing name",
+		},
+		{
+			Name: "step_missing_commands",
+			Item: &Step{
+				Name: "Setup",
+			},
+			ErrorSubstring: "missing commands",
+		},
+		{
+			Name: "package_missing_version",
+			Item: &Package{
+				Repository: GithubRepository{Owner: "octocat", Name: "hello-world"},
+			},
+			ErrorSubstring: "missing version",
+		},
+		{
+			Name: "package_missing_module",
+			Item: &Package{
+				Repository: GithubRepository{Owner: "octocat", Name: "hello-world"},
+				Version:    "v1.0.1",
+			},
+			ErrorSubstring: "missing module",
+		},
+		{
+			Name: "package_no_generate_commands",
+			Item: &Package{
+				Repository: GithubRepository{Owner: "octocat", Name: "hello-world"},
+				Version:    "v1.0.1",
+				Module:     "avo/go.mod",
+			},
+			ErrorSubstring: "no generate commands",
+		},
+		{
+			Name: "package_invalid_generate_commands",
+			Item: &Package{
+				Repository: GithubRepository{Owner: "octocat", Name: "hello-world"},
+				Version:    "v1.0.1",
+				Module:     "avo/go.mod",
+				Generate: []*Step{
+					{},
+				},
+			},
+			ErrorSubstring: "generate step: missing name",
+		},
+		{
+			Name: "packages_invalid_package",
+			Item: Packages{
+				{
+					Repository: GithubRepository{Owner: "octocat", Name: "hello-world"},
+				},
+			},
+			ErrorSubstring: "missing version",
+		},
+	}
+	for _, c := range cases {
+		c := c // scopelint
+		t.Run(c.Name, func(t *testing.T) {
+			err := c.Item.Validate()
+			if err == nil {
+				t.Fatal("expected error; got nil")
+			}
+			if !strings.Contains(err.Error(), c.ErrorSubstring) {
+				t.Fatalf("expected error message to contain %q; got %q", c.ErrorSubstring, err)
+			}
+		})
+	}
+}
+
 func TestLoadPackagesBad(t *testing.T) {
 	r := strings.NewReader(`[{"unknown_field": "value"}]`)
 	_, err := LoadPackages(r)
