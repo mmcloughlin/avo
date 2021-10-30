4f7179b08e3eec5f599905e16c94b4dceab7fc9a
Michael McLoughlin <mmcloughlin@gmail.com>
2021-10-30 13:47:55 -0700

    gotypes,tests: test case for issue #195 (#214)
    Adds test cases for the bug in #195, similar to #212.
    
    Updates #195
    Updates #191

---
+++ tests/fixedbugs/issue195/asm.go
@@ -0,0 +1,15 @@
+//go:build ignore
+// +build ignore
+
+package main
+
+import (
+	. "github.com/mmcloughlin/avo/build"
+)
+
+func main() {
+	TEXT("Issue195", NOSPLIT|NOFRAME, "func(x *uint64, y uint32)")
+	Doc("Issue195 tests for correct argument size.")
+	RET()
+	Generate()
+}
---
+++ tests/fixedbugs/issue195/doc.go
@@ -0,0 +1,3 @@
+// Package issue195 tests for correct argument size for a function without
+// return types. This test is closely related to issue #191.
+package issue195
