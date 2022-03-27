616c584f5c11b919c07829f494ed17239940fa63
Michael McLoughlin <mmcloughlin@gmail.com>
2022-03-27 12:57:27 -0700

    ci: bump to go 1.18

--- tests/thirdparty/make_workflow.go
+++ tests/thirdparty/make_workflow.go
@@ -103,7 +103,7 @@ func GenerateWorkflow(pkgs thirdparty.Packages) ([]byte, error) {
 		g.Linef("- name: Install Go")
 		g.Linef("  uses: actions/setup-go@37335c7bb261b353407cff977110895fa0b4f7d8 # v2.1.3")
 		g.Linef("  with:")
-		g.Linef("    go-version: 1.17.x")
+		g.Linef("    go-version: 1.18.x")
 
 		// Checkout avo.
 		avodir := "avo"
