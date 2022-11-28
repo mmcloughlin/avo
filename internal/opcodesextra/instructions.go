69eb79467b704b6959f2cf5038377e2c37626cca
Michael McLoughlin <mmcloughlin@gmail.com>
2022-11-27 18:40:33 -0800

    note on the expectations for extras list

--- internal/opcodesextra/instructions.go
+++ internal/opcodesextra/instructions.go
@@ -10,6 +10,13 @@ var sets = [][]*inst.Instruction{
 }
 
 // Instructions returns a list of extras to add to the instructions database.
+//
+// Note that instructions returned are expected to be injected into the loading
+// process, as if they had been read out of the Opcodes database.  As such, they
+// are not expected to be in the final form required for the instruction
+// database. For example, AVX-512 instruction form transformations do not need
+// to be applied, and operand types such as xmm{k}{z} or m256/m64bcst may be
+// used for brevity.
 func Instructions() []*inst.Instruction {
 	// Concatenate and clone the instruction lists.  It can be convenient for
 	// forms lists and other data structures to be shared in the curated lists,
