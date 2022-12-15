42874d6aebef87ec335f4cbc771bb00a06c0efc3
Vaughn Iverson <vsivsi@yahoo.com>
2022-12-14 17:34:10 -0800

    Fixed package comment in doc.go

--- tests/fixedbugs/issue193/doc.go
+++ tests/fixedbugs/issue193/doc.go
@@ -1,7 +1,8 @@
 // Package issue193 tests for the ability to generate VPSCATTER{D,Q}{D,Q} and
 // VPGATHER{D,Q}{D,Q} instructions *without* specifying a base register in
 // the destination/source VSIB addressing (VM{64,32}{x,y,z}) memory operands.
-//
+package issue193
+
 // From: https://www.felixcloutier.com/x86/vpscatterdd:vpscatterdq:vpscatterqd:vpscatterqq
 //
 // BASE_ADDR stands for the memory operand base address (a GPR); *may not exist*
@@ -10,9 +11,11 @@
 // DISP is the optional 1 or 4 byte displacement
 //
 // For example, prior to this fix Avo requires:
-//     VPSCATTERQQ Z6, K1, 8(BX)(Z7*1)
+//
+// VPSCATTERQQ Z6, K1, 8(BX)(Z7*1)
+//
 // with BX = 0, when:
-//     VPSCATTERQQ Z6, K1, 8(Z7*1)
+//
+// VPSCATTERQQ Z6, K1, 8(Z7*1)
+//
 // is perfectly valid and will suffice.
-
-package issue193
