170fe8acac153f1c2233f0ef7690b94a4e4c9b6f
Vaughn Iverson <vsivsi@yahoo.com>
2022-12-14 17:25:54 -0800

    More whitespace eaten by linter

--- tests/fixedbugs/issue193/issue193_test.go
+++ tests/fixedbugs/issue193/issue193_test.go
@@ -9,7 +9,6 @@ import (
 //go:generate go run asm.go -out issue193.s -stubs stub.go
 
 func TestAddSubs(t *testing.T) {
-
 	if !(cpu.X86.HasAVX2 && cpu.X86.HasAVX512F && cpu.X86.HasAVX512DQ && cpu.X86.HasAVX512VL) {
 		t.Skipf("Skipping issue193 test because runtime lacks needed CPUID features")
 	}
