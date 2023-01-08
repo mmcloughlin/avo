40772e7a9be499559c0909a6912e388431dd3de3
Michael McLoughlin <mmcloughlin@gmail.com>
2023-01-08 11:20:27 -0800

    tweaks

--- internal/opcodesextra/vnni.go
+++ internal/opcodesextra/vnni.go
@@ -6,57 +6,56 @@ import "github.com/mmcloughlin/avo/internal/inst"
 var vnni = []*inst.Instruction{
 	// Reference: https://github.com/golang/go/blob/go1.19.3/src/cmd/internal/obj/x86/avx_optabs.go#L3056-L3075
 	//
-	//	{as: AVPDPBUSD, ytab: _yvblendmpd, prefix: Pavx, op: opBytes{
-	//		avxEscape | evex128 | evex66 | evex0F38 | evexW0, evexN16 | evexBcstN4 | evexZeroingEnabled, 0x50,
-	//		avxEscape | evex256 | evex66 | evex0F38 | evexW0, evexN32 | evexBcstN4 | evexZeroingEnabled, 0x50,
-	//		avxEscape | evex512 | evex66 | evex0F38 | evexW0, evexN64 | evexBcstN4 | evexZeroingEnabled, 0x50,
-	//	}},
-	//	{as: AVPDPBUSDS, ytab: _yvblendmpd, prefix: Pavx, op: opBytes{
-	//		avxEscape | evex128 | evex66 | evex0F38 | evexW0, evexN16 | evexBcstN4 | evexZeroingEnabled, 0x51,
-	//		avxEscape | evex256 | evex66 | evex0F38 | evexW0, evexN32 | evexBcstN4 | evexZeroingEnabled, 0x51,
-	//		avxEscape | evex512 | evex66 | evex0F38 | evexW0, evexN64 | evexBcstN4 | evexZeroingEnabled, 0x51,
-	//	}},
-	//	{as: AVPDPWSSD, ytab: _yvblendmpd, prefix: Pavx, op: opBytes{
-	//		avxEscape | evex128 | evex66 | evex0F38 | evexW0, evexN16 | evexBcstN4 | evexZeroingEnabled, 0x52,
-	//		avxEscape | evex256 | evex66 | evex0F38 | evexW0, evexN32 | evexBcstN4 | evexZeroingEnabled, 0x52,
-	//		avxEscape | evex512 | evex66 | evex0F38 | evexW0, evexN64 | evexBcstN4 | evexZeroingEnabled, 0x52,
-	//	}},
-	//	{as: AVPDPWSSDS, ytab: _yvblendmpd, prefix: Pavx, op: opBytes{
-	//		avxEscape | evex128 | evex66 | evex0F38 | evexW0, evexN16 | evexBcstN4 | evexZeroingEnabled, 0x53,
-	//		avxEscape | evex256 | evex66 | evex0F38 | evexW0, evexN32 | evexBcstN4 | evexZeroingEnabled, 0x53,
-	//		avxEscape | evex512 | evex66 | evex0F38 | evexW0, evexN64 | evexBcstN4 | evexZeroingEnabled, 0x53,
-	//	}},
+	//		{as: AVPDPBUSD, ytab: _yvblendmpd, prefix: Pavx, op: opBytes{
+	//			avxEscape | evex128 | evex66 | evex0F38 | evexW0, evexN16 | evexBcstN4 | evexZeroingEnabled, 0x50,
+	//			avxEscape | evex256 | evex66 | evex0F38 | evexW0, evexN32 | evexBcstN4 | evexZeroingEnabled, 0x50,
+	//			avxEscape | evex512 | evex66 | evex0F38 | evexW0, evexN64 | evexBcstN4 | evexZeroingEnabled, 0x50,
+	//		}},
+	//		{as: AVPDPBUSDS, ytab: _yvblendmpd, prefix: Pavx, op: opBytes{
+	//			avxEscape | evex128 | evex66 | evex0F38 | evexW0, evexN16 | evexBcstN4 | evexZeroingEnabled, 0x51,
+	//			avxEscape | evex256 | evex66 | evex0F38 | evexW0, evexN32 | evexBcstN4 | evexZeroingEnabled, 0x51,
+	//			avxEscape | evex512 | evex66 | evex0F38 | evexW0, evexN64 | evexBcstN4 | evexZeroingEnabled, 0x51,
+	//		}},
+	//		{as: AVPDPWSSD, ytab: _yvblendmpd, prefix: Pavx, op: opBytes{
+	//			avxEscape | evex128 | evex66 | evex0F38 | evexW0, evexN16 | evexBcstN4 | evexZeroingEnabled, 0x52,
+	//			avxEscape | evex256 | evex66 | evex0F38 | evexW0, evexN32 | evexBcstN4 | evexZeroingEnabled, 0x52,
+	//			avxEscape | evex512 | evex66 | evex0F38 | evexW0, evexN64 | evexBcstN4 | evexZeroingEnabled, 0x52,
+	//		}},
+	//		{as: AVPDPWSSDS, ytab: _yvblendmpd, prefix: Pavx, op: opBytes{
+	//			avxEscape | evex128 | evex66 | evex0F38 | evexW0, evexN16 | evexBcstN4 | evexZeroingEnabled, 0x53,
+	//			avxEscape | evex256 | evex66 | evex0F38 | evexW0, evexN32 | evexBcstN4 | evexZeroingEnabled, 0x53,
+	//			avxEscape | evex512 | evex66 | evex0F38 | evexW0, evexN64 | evexBcstN4 | evexZeroingEnabled, 0x53,
+	//		}},
 	//
-	// **NOTE** The Golang assembler does not support the VEX encoded "AVX-VNNI" forms of these instructions
-
+	// Note the Go assembler does not support the VEX encoded "AVX-VNNI" forms of these instructions.
 	{
 		Opcode:  "VPDPBUSD",
 		Summary: "Multiply and Add Unsigned and Signed Bytes",
-		Forms:   vnniForms,
+		Forms:   vnniforms,
 	},
 	{
 		Opcode:  "VPDPBUSDS",
 		Summary: "Multiply and Add Unsigned and Signed Bytes with Saturation",
-		Forms:   vnniForms,
+		Forms:   vnniforms,
 	},
 	{
 		Opcode:  "VPDPWSSD",
 		Summary: "Multiply and Add Signed Word Integers",
-		Forms:   vnniForms,
+		Forms:   vnniforms,
 	},
 	{
 		Opcode:  "VPDPWSSDS",
 		Summary: "Multiply and Add Signed Word Integers with Saturation",
-		Forms:   vnniForms,
+		Forms:   vnniforms,
 	},
 }
 
 // VPDPBUSD, VPDPBUSDS, VPDPWSSD and VPDPWSSDS forms.
 //
 // See: https://www.felixcloutier.com/x86/vpdpbusd
-// 		https://www.felixcloutier.com/x86/vpdpbusds
-// 		https://www.felixcloutier.com/x86/vpdpwssd
-// 		https://www.felixcloutier.com/x86/vpdpwssds
+// See: https://www.felixcloutier.com/x86/vpdpbusds
+// See: https://www.felixcloutier.com/x86/vpdpwssd
+// See: https://www.felixcloutier.com/x86/vpdpwssds
 //
 // Reference: https://github.com/golang/go/blob/go1.19.3/src/cmd/internal/obj/x86/avx_optabs.go#L148-L155
 //
@@ -68,8 +67,7 @@ var vnni = []*inst.Instruction{
 //		{zcase: Zevex_rm_v_r, zoffset: 0, args: argList{Yzm, Yzr, Yzr}},
 //		{zcase: Zevex_rm_v_k_r, zoffset: 3, args: argList{Yzm, Yzr, Yknot0, Yzr}},
 //	}
-
-var vnniForms = inst.Forms{
+var vnniforms = inst.Forms{
 	// EVEX.128.66.0F38.W0 50 /r VPDPBUSD xmm1{k1}{z}, xmm2, xmm3/m128/m32bcst  AVX512VNNI AVX512VL
 	// EVEX.128.66.0F38.W0 51 /r VPDPBUSDS xmm1{k1}{z}, xmm2, xmm3/m128/m32bcst  AVX512VNNI AVX512VL
 	// EVEX.128.66.0F38.W0 52 /r VPDPWSSD xmm1{k1}{z}, xmm2, xmm3/m128/m32bcst  AVX512VNNI AVX512VL
