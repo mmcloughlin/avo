golang/go

commit 2da95e0ec80cb7df1f89a7b7f147dde42ad17a19

go/types, types2: report "undefined: x" instead of "undeclared name: x"
This matches the compiler's long-standing behavior.

For #55326.

Change-Id: I90696a11f0b7d1f4be95a4b9a6f01844df2a2347
Reviewed-on: https://go-review.googlesource.com/c/go/+/432555
Run-TryBot: Robert Findley <rfindley@google.com>
Reviewed-by: Robert Griesemer <gri@google.com>
Reviewed-by: Robert Findley <rfindley@google.com>
TryBot-Result: Gopher Robot <gobot@golang.org>

src/cmd/compile/internal/types2/issues_test.go
@@ -28,7 +28,7 @@ func TestIssue5770(t *testing.T) {
 	f := mustParse(t, `package p; type S struct{T}`)
 	var conf Config
 	_, err := conf.Check(f.PkgName.Value, []*syntax.File{f}, nil) // do not crash
-	want := "undeclared name: T"
+	const want = "undefined: T"
 	if err == nil || !strings.Contains(err.Error(), want) {
 		t.Errorf("got: %v; want: %s", err, want)
 	}

src/cmd/compile/internal/types2/typexpr.go
@@ -37,11 +37,7 @@ func (check *Checker) ident(x *operand, e *syntax.Name, def *Named, wantType boo
 				check.error(e, _InvalidBlank, "cannot use _ as value or type")
 			}
 		} else {
-			if check.conf.CompilerErrorMessages {
-				check.errorf(e, _UndeclaredName, "undefined: %s", e.Value)
-			} else {
-				check.errorf(e, _UndeclaredName, "undeclared name: %s", e.Value)
-			}
+			check.errorf(e, _UndeclaredName, "undefined: %s", e.Value)
 		}
 		return
 	case universeAny, universeComparable:
@@ -482,7 +478,7 @@ func (check *Checker) arrayLength(e syntax.Expr) int64 {
 	if name, _ := e.(*syntax.Name); name != nil {
 		obj := check.lookup(name.Value)
 		if obj == nil {
-			check.errorf(name, _InvalidArrayLen, "undeclared name %s for array length", name.Value)
+			check.errorf(name, _InvalidArrayLen, "undefined %s for array length", name.Value)
 			return -1
 		}
 		if _, ok := obj.(*Const); !ok {

src/go/types/issues_test.go
@@ -31,7 +31,7 @@ func TestIssue5770(t *testing.T) {
 	f := mustParse(t, `package p; type S struct{T}`)
 	conf := Config{Importer: importer.Default()}
 	_, err := conf.Check(f.Name.Name, fset, []*ast.File{f}, nil) // do not crash
-	want := "undeclared name: T"
+	const want = "undefined: T"
 	if err == nil || !strings.Contains(err.Error(), want) {
 		t.Errorf("got: %v; want: %s", err, want)
 	}

src/go/types/typexpr.go
@@ -38,7 +38,7 @@ func (check *Checker) ident(x *operand, e *ast.Ident, def *Named, wantType bool)
 				check.error(e, _InvalidBlank, "cannot use _ as value or type")
 			}
 		} else {
-			check.errorf(e, _UndeclaredName, "undeclared name: %s", e.Name)
+			check.errorf(e, _UndeclaredName, "undefined: %s", e.Name)
 		}
 		return
 	case universeAny, universeComparable:
@@ -469,7 +469,7 @@ func (check *Checker) arrayLength(e ast.Expr) int64 {
 	if name, _ := e.(*ast.Ident); name != nil {
 		obj := check.lookup(name.Name)
 		if obj == nil {
-			check.errorf(name, _InvalidArrayLen, "undeclared name %s for array length", name.Name)
+			check.errorf(name, _InvalidArrayLen, "undefined %s for array length", name.Name)
 			return -1
 		}
 		if _, ok := obj.(*Const); !ok {

src/internal/types/testdata/check/constdecl.go
@@ -87,7 +87,7 @@ func _() {
 // Caused panic because the constant value was not set up (gri - 7/8/2014).
 func _() {
 	const (
-	    x string = missing /* ERROR "undeclared name" */
+	    x string = missing /* ERROR "undefined" */
 	    y = x + ""
 	)
 }
@@ -97,11 +97,11 @@ const A /* ERROR initialization cycle */ = unsafe.Sizeof(func() { _ = A })
 
 func _() {
 	// The function literal below must not see a.
-	const a = unsafe.Sizeof(func() { _ = a /* ERROR "undeclared name" */ })
+	const a = unsafe.Sizeof(func() { _ = a /* ERROR "undefined" */ })
 	const b = unsafe.Sizeof(func() { _ = a })
 
 	// The function literal below must not see x, y, or z.
-	const x, y, z = 0, 1, unsafe.Sizeof(func() { _ = x /* ERROR "undeclared name" */ + y /* ERROR "undeclared name" */ + z /* ERROR "undeclared name" */ })
+	const x, y, z = 0, 1, unsafe.Sizeof(func() { _ = x /* ERROR "undefined" */ + y /* ERROR "undefined" */ + z /* ERROR "undefined" */ })
 }
 
 // Test cases for errors in inherited constant initialization expressions.

src/internal/types/testdata/check/cycles0.go
@@ -86,8 +86,8 @@ func _() {
 		t1 /* ERROR invalid recursive type */ t1
 		t2 *t2
 
-		t3 t4 /* ERROR undeclared */
-		t4 t5 /* ERROR undeclared */
+		t3 t4 /* ERROR undefined */
+		t4 t5 /* ERROR undefined */
 		t5 t3
 
 		// arrays

src/internal/types/testdata/check/decls0.go
@@ -13,7 +13,7 @@ import "unsafe"
 const pi = 3.1415
 
 type (
-	N undeclared /* ERROR "undeclared" */
+	N undefined /* ERROR "undefined" */
 	B bool
 	I int32
 	A [10]P

src/internal/types/testdata/check/decls2/decls2a.go
@@ -47,8 +47,8 @@ type T2 struct {
 }
 
 // Methods declared without a declared type.
-func (undeclared /* ERROR "undeclared" */) m() {}
-func (x *undeclared /* ERROR "undeclared" */) m() {}
+func (undefined /* ERROR "undefined" */) m() {}
+func (x *undefined /* ERROR "undefined" */) m() {}
 
 func (pi /* ERROR "not a type" */) m1() {}
 func (x pi /* ERROR "not a type" */) m2() {}

mmcloughlin/avo

commit dcba6827a0d2e3d52a41121a803f6364e7672a42

gotypes: handle change in error message in go 1.20
Uses a regular expression to handle the change in error message
introduced in:

https://github.com/golang/go/commit/2da95e0ec80cb7df1f89a7b7f147dde42ad17a19

Updates #367

gotypes/signature_test.go
@@ -3,7 +3,7 @@ package gotypes
 import (
 	"go/token"
 	"go/types"
-	"strings"
+	"regexp"
 	"testing"
 
 	"golang.org/x/tools/go/packages"
@@ -118,17 +118,22 @@ func TestParseSignature(t *testing.T) {
 
 func TestParseSignatureErrors(t *testing.T) {
 	cases := []struct {
-		Expr          string
-		ErrorContains string
+		Expr         string
+		ErrorPattern string
 	}{
-		{"idkjklol", "undeclared name"},
-		{"struct{}", "not a function signature"},
-		{"uint32(0xfeedbeef)", "should have nil value"},
+		{"idkjklol", `(undeclared|undefined)`},
+		{"struct{}", `not a function signature`},
+		{"uint32(0xfeedbeef)", `should have nil value`},
 	}
 	for _, c := range cases {
+		errrx, err := regexp.Compile(c.ErrorPattern)
+		if err != nil {
+			t.Fatal(err)
+		}
+
 		s, err := ParseSignature(c.Expr)
-		if s != nil || err == nil || !strings.Contains(err.Error(), c.ErrorContains) {
-			t.Errorf("expect error from expression %s\ngot: %s\nexpect substring: %s\n", c.Expr, err, c.ErrorContains)
+		if s != nil || err == nil || !errrx.MatchString(err.Error()) {
+			t.Errorf("expect error from expression %s\ngot: %s\nexpect match: %s\n", c.Expr, err, c.ErrorPattern)
 		}
 	}
 }